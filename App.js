cf857562cb9f2a01d9f5484e69c3505bcb1062d7
Gregory Zabrodskiy <grzabrodskiy@gmail.com>
2022-01-18 23:35:37 +0100

    clean up

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
-import {
-  SafeAreaView,
+import { StyleSheet, Animated, View, Button, Image } from 'react-native';
 
-  TouchableOpacity,
-  Animated,
-} from 'react-native'
+
 import { Camera } from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
 
