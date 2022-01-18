63c4e06bd374ac3395fa2cb57cf421e5a2c959f3
Gregory Zabrodskiy <grzabrodskiy@gmail.com>
2022-01-18 20:40:22 +0100

    init

--- App.js
+++ App.js
@@ -1,11 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
 
-export default function App() {
+export default function Add({ navigation }) {
+  const [cameraPermission, setCameraPermission] = useState(null);
+  const [galleryPermission, setGalleryPermission] = useState(null);
+
+  const [camera, setCamera] = useState(null);
+  const [imageUri, setImageUri] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  const permisionFunction = async () => {
+    // here is how you can get the camera permission
+    const cameraPermission = await Camera.requestPermissionsAsync();
+
+    setCameraPermission(cameraPermission.status === 'granted');
+
+    const imagePermission = await ImagePicker.getMediaLibraryPermissionsAsync();
+    console.log(imagePermission.status);
+
+    setGalleryPermission(imagePermission.status === 'granted');
+
+    if (
+      imagePermission.status !== 'granted' &&
+      cameraPermission.status !== 'granted'
+    ) {
+      alert('Permission for media access needed.');
+    }
+  };
+
+  useEffect(() => {
+    permisionFunction();
+  }, []);
+
+  const takePicture = async () => {
+    if (camera) {
+      const data = await camera.takePictureAsync(null);
+      console.log(data.uri);
+      setImageUri(data.uri);
+    }
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    console.log(result);
+    if (!result.cancelled) {
+      setImageUri(result.uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.title}>
+      <h3> Welcome to the Imagine PoC</h3>
+      </View>
+      <View style={styles.cameraContainer}>
+        <Camera
+          ref={(ref) => setCamera(ref)}
+          style={styles.fixedRatio}
+          type={type}
+          ratio={'1:1'}
+        />
+      </View>
+
+      <Button title={'Take Picture'} onPress={takePicture} />
+      <Button title={'Gallery'} onPress={pickImage} />
+      {imageUri && <Image source={{ uri: imageUri }} style={styles.cameraContainer} />}
     </View>
   );
 }
@@ -13,8 +80,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  cameraContainer: {
+    flex: 0.2,
+    flexDirection: 'row',
+    borderWidth: 4,
+    borderColor: "#61DBFB",
+    borderRadius: 6,
+  },
+  fixedRatio: {
+    flex: 1,
+    aspectRatio: 1,
+  },
+  button: {
+    flex: 0.1,
+    padding: 10,
+    alignSelf: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fffde7",
+    color: "#61DBFB",
+    borderColor: "#61DBFB",
+
+  },
+  title: {
+    marginTop: 0,
+    paddingVertical: 5,
+    borderWidth: 4,
+    borderColor: "#61DBFB",
+    borderRadius: 6,
+    backgroundColor: "#fffde7",
+    color: "#61DBFB",
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold"
   },
 });
